bbf15b69f815122c182526c90f77925c81b1d744
Matt Chan <wildcatrobotics2011@gmail.com>
2019-10-25 22:12:19 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var http = require('http'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    path = require('path');
+
+var app = express();
+app.use(bodyParser.json());
+
+var audiodata = require('./fileids.json');
+
+//app.use(express.static(__dirname));
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.get('/style.css', function(req, res){
+  res.sendFile(path.join(__dirname, 'style.css'));
+})
+
+app.get('/runtest.js', function(req, res){
+  res.sendFile(path.join(__dirname, 'runtest.js'));
+})
+
+app.post('/nextaudio', function(req, res){
+  var optionlist = [];
+  var correctanswer = -1;
+  var correctanswerid = -1;
+  var filelink = "";
+
+  var past5ids = req.body.past5ids;
+  var usedids = [];
+  while(optionlist.length < 4){
+    var randindex = Math.floor(Math.random()*audiodata.names.length);
+    if(past5ids.indexOf(randindex)!=-1)continue;
+    if(usedids.indexOf(randindex)!=-1)continue;
+    usedids.push(randindex);
+    optionlist.push(audiodata.names[randindex]);
+  }
+  correctanswer = Math.floor(Math.random()*4);
+  correctanswerid = usedids[correctanswer];
+  filelink = audiodata.links[correctanswerid];
+
+  res.json({
+    optionlist,
+    correctanswer,
+    correctanswerid,
+    filelink
+  })
+})
+
+var server = http.createServer(app);
+
+server.listen(3000);
+console.log("Server started on 3000")
---
+++ runtest.js
@@ -0,0 +1,121 @@
+var optstore = [];
+optstore[0] = $('#opt0');
+optstore[1] = $('#opt1');
+optstore[2] = $('#opt2');
+optstore[3] = $('#opt3');
+
+var correctoption = -1;
+
+var past5ids = [];
+
+var musicplayer = $('#musicplayer')[0];
+
+var numtotal = 0;
+var numcorrect = 0;
+
+function getNextTest(){
+  $.ajax({
+    type: "POST",
+    url: '/nextaudio',
+    data: JSON.stringify({'past5ids': past5ids}),
+    contentType: 'application/json',
+    dataType:'json',
+    success: (data)=>{
+      optstore[0].removeClass("wrong-butt selected right-butt disable-butt")
+      optstore[1].removeClass("wrong-butt selected right-butt disable-butt")
+      optstore[2].removeClass("wrong-butt selected right-butt disable-butt")
+      optstore[3].removeClass("wrong-butt selected right-butt disable-butt")
+      optstore[0].attr("value", data.optionlist[0])
+      optstore[1].attr("value", data.optionlist[1])
+      optstore[2].attr("value", data.optionlist[2])
+      optstore[3].attr("value", data.optionlist[3])
+      past5ids.push(data.correctanswerid);
+      correctoption = data.correctanswer;
+      $('#score').html('Score: '+numcorrect+' / '+numtotal);
+      numtotal++;
+      if(past5ids.length > 5) past5ids.shift();
+      $('#sourcefile').attr("src", data.filelink)
+      musicplayer.load();
+      musicplayer.pause();
+      if($('#skip10').is(":checked")) musicplayer.currentTime = 10;
+      else musicplayer.currentTime = 0;
+      if($('#play2x').is(":checked")) musicplayer.playbackRate=2;
+      else musicplayer.playbackRate=1;
+      musicplayer.oncanplay = ()=>{
+        musicplayer.play();
+        if(!musicplayer.paused)$('#playbutt').attr('value', 'Pause');
+      }
+    },
+    error: (error)=>{
+      console.log("error loading next test")
+    }
+  });
+}
+
+function optsel(num){
+  for(var i = 0; i<4; i++){
+    if(i==correctoption){
+      optstore[i].addClass("right-butt disable-butt")
+    }else{
+      optstore[i].addClass("wrong-butt disable-butt")
+    }
+    if(i==num){
+      optstore[i].addClass("selected")
+    }
+  }
+  if(num==correctoption){
+    numcorrect++;
+  }
+}
+
+function togglePlay(){
+  if(musicplayer.paused){
+    musicplayer.play();
+    $('#playbutt').attr('value', 'Pause')
+  }else{
+    musicplayer.pause();
+    $('#playbutt').attr('value', 'Play')
+  }
+}
+
+function restart(){
+  musicplayer.currentTime = 0;
+  musicplayer.play();
+}
+
+function play2xtoggled(){
+  if($('#play2x').is(":checked")) musicplayer.playbackRate=2;
+  else musicplayer.playbackRate=1;
+}
+
+var night = false;
+
+function toggleNightMode() {
+  if(night){
+    $('*').removeClass('night');
+    document.cookie = 'nightmode=false';
+  }else{
+    $('*').addClass('night');
+    document.cookie = 'nightmode=true';
+  }
+  night = !night;
+}
+
+getNextTest();
+
+if(document.cookie.indexOf('nightmode=true')!=-1){
+  toggleNightMode();
+  $('#nightmode').prop('checked', 'true')
+}
+
+function resize(){
+  if($(window).width() < 750){
+    $('#testcol').removeClass('one-half')
+    $('#optcol').removeClass('one-half').addClass('resizefix')
+  }else{
+    $('#testcol').addClass('one-half')
+    $('#optcol').addClass('one-half').removeClass('resizefix')
+  }
+}
+resize();
+$(window).resize(resize);
